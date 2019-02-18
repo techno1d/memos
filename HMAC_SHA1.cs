959c24bdaa3c6df8a1ab11da12bf987f971e410e
techno1d <techno1d@yandex.ru>
2019-02-18 10:19:36 +0300

    Update HMAC_SHA1.cs

--- HMAC_SHA1.cs
+++ HMAC_SHA1.cs
@@ -1,8 +1,16 @@
-public static string Encode(string input, string key)
-{			
-	HMACSHA1 myhmacsha1 = new HMACSHA1(Encoding.ASCII.GetBytes(key));
-	byte[] byteArray = Encoding.ASCII.GetBytes(input);
-	
-	MemoryStream stream = new MemoryStream(byteArray);
-	return Convert.ToBase64String( myhmacsha1.ComputeHash(stream));
-}		
+using System;
+using System.Text;
+
+using System.Security.Cryptography;
+using System.IO;
+
+public static class Helper {
+	public static string Encode(string input, string key)
+	{			
+		HMACSHA1 myhmacsha1 = new HMACSHA1(Encoding.ASCII.GetBytes(key));
+		byte[] byteArray = Encoding.ASCII.GetBytes(input);
+		
+		MemoryStream stream = new MemoryStream(byteArray);
+		return Convert.ToBase64String( myhmacsha1.ComputeHash(stream));
+	}
+}
